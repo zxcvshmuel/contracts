70783e9d1c1e0c507929b936d3a0087006a41189
myusername <myemail@example.com>
2023-05-30 11:09:42 +0300

    fixes and payments

--- app/Filament/Pages/PackagesPage.php
+++ app/Filament/Pages/PackagesPage.php
@@ -3,12 +3,21 @@
 namespace App\Filament\Pages;
 
 use App\Models\Package;
+use Filament\Forms\Components\Card;
 use Filament\Pages\Page;
-use Illuminate\Contracts\View\View;
+use Filament\Resources\Form;
+use Filament\Tables\Columns\TextInputColumn;
 
 class PackagesPage extends Page
 {
     public array $data;
+    public array $dataToPay;
+
+    public bool $popup = false;
+
+
+
+    protected $listeners = ['$showPayModal', 'packageClick' => 'packageClick'];
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
@@ -17,16 +26,65 @@ class PackagesPage extends Page
     protected ?string $heading = 'החבילות שלנו';
 
 
-    public function mount()
+    public function mount(): void
     {
-        $this->data['packages'] = Package::all();
+        $this->data['packages'] = Package::all()->where('status', 1)->where('price', '>', 0);
+        $this->dataToPay = Package::find(1)->toArray();
     }
 
-    public function packageClick($id)
+    public function packageClick($id): void
     {
-        $this->data['package'] = Package::find($id);
+        $this->data['packages'] = Package::all()->where('status', 1)->where('price', '>', 0);
+        $this->dataToPay = Package::find($id)->toArray();
 
-        dd($this->data['package']);
+        $this->popup = true;
     }
+
+    public function redirectToPay($id): void
+    {
+        $package = Package::find($id);
+        $user = auth()->user();
+
+        $params = [
+            'action' => 'pay',
+            'Amount' => $package->price,
+            'ClientLName' => $user->name,
+            'ClientName' => $user->name,
+            'Coin' => 1,
+            'FixTash' => false,
+            'Info' => 'test-api',
+            'J5' => false,
+            'Masof' => '0010188625',
+            'MoreData' => true,
+            'Order' => '1',
+            'PageLang' => 'HEB',
+            'Postpone' => false,
+            'Pritim' => true,
+            'SendHesh' => true,
+            'ShowEngTashText' => false,
+            'Sign' => true,
+            'Tash' => 2,
+            'UTF8' => true,
+            'UTF8out' => true,
+            'UserId' => $user->id,
+            'cell' => $user->phone,
+            'city' => $user->comp_address,
+            'email' => $user->email,
+            'heshDesc' => '[0~Item 1~1~8][0~Item 2~2~1]',
+            'phone' => $user->phone,
+            'sendemail' => true,
+            'street' => 'levanon 3',
+            'tmp' => 1,
+            'zip' => '42361',
+            'signature' => 'b6589fc6ab0dc82cf12099d1c2d40ab994e8410c'
+        ];
+
+
+        $url = 'https://icom.yaad.net/p/?' . http_build_query($params);
+
+
+        $this->redirect($url);
+    }
+
 
 }
--- resources/views/filament/pages/packages-page.blade.php
+++ resources/views/filament/pages/packages-page.blade.php
@@ -1,19 +1,19 @@
 <x-filament::page>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            תוכניות
-        </h2>
-    </x-slot>
+    <div x-data="{popup : {{$popup}} }">
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                תוכניות
+            </h2>
+        </x-slot>
 
-    <x-filament::card>
-        <div class="grid">
-            @php
-                $count = 0;
-                $class = ['basic', 'standard', 'premium'];
-                $duration = '';
-            @endphp
-            @foreach ($this->data['packages'] as $package)
-                @if ($package->price > 0)
+        <x-filament::card>
+            <div class="grid">
+                @php
+                    $count = 0;
+                    $class = ['basic', 'standard', 'premium'];
+                    $duration = '';
+                @endphp
+                @foreach ($this->data['packages'] as $package)
                     <div style="padding: 10px 0 10px 0;" class="mycard {{ $class[$count]}}">
                         <div class="title">
                             <i class="fa fa-paper-plane" aria-hidden="true"></i>
@@ -33,19 +33,154 @@
                                 <li><i class="fa fa-check" aria-hidden="true"></i> יומן עם סנכרון לגוגל</li>
                             </ul>
                         </div>
-                        <a  wire:click="packageClick({{ $package->id }})" class="package-btn bg-blue-600 hover:bg-blue-700 text-white font-bold my-6 py-2 px-4 border border-blue-700 rounded">המשך</a>
+                        <button type="button" data-modal-target="popup-modal" data-modal-toggle="popup-modal"
+                                wire:click="packageClick({{ $package->id }})"
+                                class="package-btn bg-blue-700 hover:bg-blue-700 text-white font-bold my-6 py-2 px-4 border border-blue-700 rounded">
+                            המשך
+                        </button>
                     </div>
-        @endif
-        @php
-            $count++;
-            if ($count == 3) {
-                $count = 0;
-            }
-        @endphp
-        @endforeach
-    </x-filament::card>
+                    @php
+                        $count++;
+                        if ($count == 3) {
+                            $count = 0;
+                        }
+                    @endphp
+                @endforeach
+            </div>
+
+        </x-filament::card>
+        <div x-show="popup">
+            <div id="popup-modal" tabindex="-1"
+                 class="fixed top-0 left-0 right-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative m-auto w-full max-w-md max-h-full">
+                    <div class="relative m-auto bg-white rounded-lg shadow dark:bg-gray-700">
+                        <button x-on:click=" open = false" type="button"
+                                class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                                data-modal-hide="popup-modal">
+                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                      clip-rule="evenodd"></path>
+                            </svg>
+                            <span class="sr-only">סגור</span>
+                        </button>
+                        <div class="p-8 text-center">
+                            <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white border-b rounded-t">פרטי העסקה</h3>
+
+                            <p class="text-sm font-normal text-gray-500 dark:text-gray-400">Connect with one of our available wallet providers or create a new one.</p>
+                            <ul class="my-4 space-y-3">
+                                <li>
+                                    <a href="#" class="flex flex-row-reverse items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                                        <span class="flex-1 ml-3 whitespace-nowrap">{{ $this->dataToPay['name'] ?? '' }}</span>
+                                        <span class="items-center justify-center px-2 py-0.5 ml-3 text-md font-medium text-gray-500
+                                        rounded dark:bg-gray-700 dark:text-gray-400">שם</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="flex flex-row-reverse items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                                        <span class="flex-1 ml-3 whitespace-nowrap">{{ $this->dataToPay['duration'] ?? '' }}</span>
+                                        <span class=" items-center justify-center px-2 py-0.5 ml-3 text-md font-medium text-gray-500
+                                        rounded dark:bg-gray-700 dark:text-gray-400">משך</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="flex flex-row-reverse items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+                                        <span class="flex-1 ml-3 whitespace-nowrap">{{ $dataToPay['price'] ?? '' }}</span>
+                                        <span class=" items-center justify-center px-2 py-0.5 ml-3 text-md font-medium text-gray-500
+                                        rounded dark:bg-gray-700 dark:text-gray-400">מחיר</span>
+                                    </a>
+                                </li>
+                            </ul>
+
+
+                            <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">בחר אמצעי תשלום</h3>
+                            <form class="space-y-8" >
+                                <input type="hidden" name="package-id" value="{{ $dataToPay['id'] ?? 0 }}">
+                                <ul class="flex-column">
+                                    <li  class="pb-5" id="li-paypal">
+                                        <input type="radio" id="payment-type-paypal" name="payment" value="paypal"
+                                               class="hidden peer" required>
+                                        <label checked x-on:click="open = true" for="payment-type-paypal"
+                                               class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                            <div class="">
+                                                <div style="font-size: 24px;" class="w-full text-lg font-semibold">
+                                                    פייפל
+                                                </div>
+                                            </div>
+                                            <img style="height: 50px;"
+                                                 src="{{ \Illuminate\Support\Facades\Storage::url('/') . 'layout/payPal.png' }}"
+                                                 alt="">
+                                        </label>
+                                    </li>
+                                    <li id="li-visa">
+                                        <input type="radio" id="payment-type-visa" name="payment" value="visa"
+                                               class="hidden peer" required>
+                                        <label x-on:click="open = true" for="payment-type-visa"
+                                               class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
+                                            <div class="">
+                                                <div style="font-size: 24px;" class="w-full text-lg font-semibold">
+                                                    אשראי
+                                                </div>
+                                            </div>
+                                            <img style="height: 50px;"
+                                                 src="{{ \Illuminate\Support\Facades\Storage::url('/') . 'layout/credit.png' }}"
+                                                 alt="">
+                                        </label>
+                                    </li>
+                                </ul>
+                                <div class="flex justify-end" x-show="open" x-transition>
+                                    <button wire:click="redirectToPay({{$dataToPay['id']}})" type="submit"  id="popup-true"
+                                            class="text-white-500 focus:ring-4 focus:outline-none focus:ring-green-300 rounded-lg border border-gray-200 text-sm-center w-50 font-medium text-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:focus:ring-green-800 dark:hover:bg-green-600 inline-flex items-center  text-center mr-2">
+                                        המשך לתשלום
+                                    </button>
+
+                                    {{--<button x-on:click="open = false" id="popup-false" data-modal-hide="popup-modal" type="button"
+                                            class="text-white-500  focus:ring-4 focus:outline-none focus:ring-red-300 rounded-lg border border-gray-200 text-sm-center w-50 font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-600 inline-flex items-center  text-center mr-2">
+                                        חזור לחבילות
+                                    </button>--}}
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
     <style>
         @import url('https://fonts.googleapis.com/css?family=Roboto:300');
+
+        #popup-true {
+            background-color: #4CAF50;
+        }
+
+        #popup-true:hover {
+            background-color: #1cd020;
+        }
+
+        #popup-true:focus {
+            --tw-ring-color: rgb(199, 255, 199);
+        }
+
+        #popup-false {
+            background-color: #c41616;
+            color: white;
+        }
+
+        #popup-false:hover {
+            background-color: #f60f0f;
+            color: white;
+        }
+
+        #popup-false:focus {
+            background-color: #f60f0f;
+            color: white;
+            --tw-ring-color: rgba(255, 170, 170, 0.6);
+        }
+
 
         .package-btn:hover {
             background-color: rgb(112, 232, 133);
@@ -134,7 +269,7 @@
             width: 100%;
             height: 40%;
             background: rgba(255, 255, 255, 0.1);
-            z-index: 1;
+            z-index: -1;
             transform: skewY(-5deg) scale(1.5);
         }
 
@@ -187,7 +322,7 @@
             font-size: 16px;
         }
 
-        .mycard a {
+        .mycard button {
             position: relative;
             z-index: 2;
             border-radius: 8px;
@@ -196,7 +331,16 @@
             box-shadow: 0 5px 10px rgba(0, 0, 0, 0.20);
             font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
             font-weight: bold;
+            background-color: #7979e8;
+            border: #7979e8;
         }
 
     </style>
+    @push('scripts')
+        <script>
+            window.addEventListener('showPayModal', function (event, data) {
+                console.log(event.detail);
+            })
+        </script>
+    @endpush
 </x-filament::page>
